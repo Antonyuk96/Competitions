2ca10266b2dfcfa51b3fd05bbad0049543a43dc4
Antonyuk96 <113938370+Antonyuk96@users.noreply.github.com>
2023-03-05 23:50:33 +0300

    Add files via upload

---
+++ ConsoleOutputProvider.java
@@ -0,0 +1,7 @@
+
+public class ConsoleOutputProvider implements IOutputProvider{
+    //Вывод информации в консоль
+    public void print(String message) {
+        System.out.println(message);
+    }
+}
